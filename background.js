e02abba1586f1609cfe52eb83175c3f2d4f9a100
msdjl <msdjl@mail.ru>
2014-12-28 16:12:44 +0300

    navigation

--- background.js
+++ background.js
@@ -2,11 +2,15 @@ var currentContext = {};
 var serviceUrl = 'https://msdjl.ru';
 document.addEventListener('DOMContentLoaded', function () {
 	document.querySelector('#logout').addEventListener('click', function () {
+		$('#overlap').show();
 		$.ajax({
 			type: 'POST',
 			url: serviceUrl + '/logout',
 			data: {},
-			success: function (resp) {
+			complete: function (resp) {
+				$('#overlap').hide();
+				$('.mainForm').hide();
+				$('.loginForm').show();
 				console.log(resp);
 				sendMessage({msg: 'testService', response: resp});
 			},
@@ -21,29 +25,26 @@ document.addEventListener('DOMContentLoaded', function () {
 			currentContext = data;
 			currentContext.issue = $('#context').attr('value');
 		});
+		$('.mainForm').hide();
+		$('.statusForm').show();
+	});
+	document.querySelector('#changecontext').addEventListener('click', function () {
+		localStorage.context = '';
+		$('.statusForm').hide();
+		$('.mainForm').show();
 	});
 	document.querySelector('#loginButton').addEventListener('click', function () {
 		var u = document.querySelector('#username').value;
 		var p = document.querySelector('#password').value;
-		/*var url = serviceUrl + '/login';
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", url, true);
-		xhr.withCredentials = true;
-		xhr.setRequestHeader('Content-Type', 'application/json');
-		xhr.onloadend = function() {
-			if (xhr.readyState == 4) {
-				var resp = xhr.responseText;
-				console.log(resp);
-				//setCookie('connect.sid', resp)
-				sendMessage({msg: 'testService', response: resp});
-			}
-		};
-		xhr.send(JSON.stringify({username: u, password: p}));*/
+		$('#overlap').show();
 		$.ajax({
 			type: 'POST',
 			url: serviceUrl + '/login',
 			data: {username: u, password: p},
-			success: function (resp) {
+			complete: function (resp) {
+				$('#overlap').hide();
+				$('.loginForm').hide();
+				$('.mainForm').show();
 				console.log(resp);
 				sendMessage({msg: 'testService', response: resp});
 			},
@@ -58,17 +59,19 @@ document.addEventListener('DOMContentLoaded', function () {
 	});
 	$('#context').val(localStorage.context);
 	isAuthorized(function(xhr) {
+		$('#overlap').hide();
 		if (xhr.status == 401) {
 			$('.loginForm').show();
-			$('#overlap').hide();
 		}
 		else {
-			$('.mainForm').show();
-			$('#overlap').hide();
+			if (localStorage.context) {
+				$('.statusForm').show();
+			}
+			else {
+				$('.mainForm').show();
+			}
 		}
 	});
-	//$('.statusForm').show();
-	//$('.mainForm').show();
 });
 
 function isAuthorized (cb) {
