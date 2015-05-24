f5c65351c874dddeae501d5023a9a4c855cb59cd
msdjl <msdjl@mail.ru>
2015-05-24 21:23:03 +0300

    dist gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-	jade = require('gulp-jade');
+	jade = require('gulp-jade'),
+	del = require('del');
 
 gulp.task('jade', function() {
 	gulp.src(['./html/popup.jade', './html/buttons.jade'])
@@ -9,9 +10,23 @@ gulp.task('jade', function() {
 		.on('error', console.log)
 		.pipe(gulp.dest('./html/'));
 });
-gulp.task('watch', function () {
-	gulp.start('jade');
-	gulp.watch('html/*.jade', function () {
-		gulp.start('jade');
+gulp.task('dist', function() {
+	del(['dist/*'], function () {
+		gulp.src(['./html/*.html'])
+			.pipe(gulp.dest('dist/html'));
+		gulp.src(['css/*'])
+			.pipe(gulp.dest('dist/css'));
+		gulp.src(['img/*'])
+			.pipe(gulp.dest('dist/img'));
+		gulp.src(['js/*'])
+			.pipe(gulp.dest('dist/js'));
+		gulp.src(['manifest.json'])
+			.pipe(gulp.dest('dist'));
+		gulp.src(['bower/bootstrap/dist/**/*'])
+			.pipe(gulp.dest('dist/bower/bootstrap/dist'));
+		gulp.src(['bower/html2canvas/build/*'])
+			.pipe(gulp.dest('dist/bower/html2canvas/build'));
+		gulp.src(['bower/jquery/dist/*'])
+			.pipe(gulp.dest('dist/bower/jquery/dist'));
 	});
 });
